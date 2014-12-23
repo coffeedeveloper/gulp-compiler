86dd090cf2e551adb5f767016d05ac72198fa80c
CoffeeDeveloper <coffeedeveloper@gmail.com>
2014-12-23 11:07:25 +0800

    add sourcemap and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-var gulp = require('gulp')
-var stylus = require('gulp-stylus')
-var watch = require('gulp-watch')
+var gulp         = require('gulp')
+var stylus       = require('gulp-stylus')
+var watch        = require('gulp-watch')
+var uglify       = require('gulp-uglify')
+var minifyCSS    = require('gulp-minify-css')
+var sourcemaps   = require('gulp-sourcemaps')
+var autoprefixer = require('gulp-autoprefixer')
+var concat       = require('gulp-concat')
 
 gulp.task('compile', function() {
   gulp.src('./content/stylus/*.styl')
       .pipe(stylus())
+      .pipe(sourcemaps.init())
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('./content/dist'))
 })
 
