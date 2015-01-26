28ac1f3b9f892a9baf0ff848a8fe17fb2569164c
CoffeeDeveloper <coffeedeveloper@gmail.com>
2015-01-26 14:04:47 +0800

    update

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
 var config = {
   path: {
     source: './content/stylus',
-    dist: './content/dist'
-  }
+    dist: './content/dist',
+    import: 'import/*.styl'
+  },
+  bundles: [
+    {
+      name: 'site.css',
+      files: ['base.styl', 'site.styl']
+    },
+    {
+      name: 'app.css',
+      files: ['base.styl', 'app.styl']
+    }
+  ]
 }
 
 module.exports = config
--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,49 @@ var minifyCSS    = require('gulp-minify-css')
 var sourcemaps   = require('gulp-sourcemaps')
 var autoprefixer = require('gulp-autoprefixer')
 var concat       = require('gulp-concat')
+var rename       = require('gulp-rename')
 var path         = require('path')
-var config       = require('./config')
 
+var config     = require('./config')
+var sourceFiles = path.join(config.path.source, '*.styl')
+
+var bundles = config.bundles
 
 gulp.task('compile', function() {
-  gulp.src(path.join(config.path.source, '*.styl'))
+  gulp.src(['content/stylus/base.styl', 'content/stylus/app.styl'])
       .pipe(stylus())
-      .pipe(sourcemaps.init())
-      .pipe(autoprefixer())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(config.path.dist))
+      .pipe(concat('app.css'))
+      .pipe(gulp.dest('content/dist'))
+
+  // bundles.forEach(function(bundle) {
+  //   var files = []
+  //   bundle.files.forEach(function(f, i, arr) {
+  //     files.push(path.join(config.path.source, f))
+  //   })
+  //   console.log(files)
+  //   gulp.src(files)
+  //       .pipe(stylus())
+  //       .pipe(rename(bundle.name))
+  //       .pipe(gulp.dest(config.path.dist))
+  // })
+  // gulp.src(sourceFiles)
+  //     .pipe(stylus({
+  //       import: config.path.import,
+  //       sourcemap: {
+  //         inline: true,
+  //         sourceRoot: '.',
+  //         basePath: config.path.dist
+  //       }
+  //     }))
+  //     .pipe(sourcemaps.init({
+  //       loadMaps: true
+  //     }))
+  //     .pipe(autoprefixer())
+  //     .pipe(sourcemaps.write('.', {
+  //       includeContent: true,
+  //       sourceRoot: '.'
+  //     }))
+  //     .pipe(gulp.dest(config.path.dist))
 })
 
 gulp.watch('./content/stylus/*.styl', ['compile'])
