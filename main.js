8749ca7c452ccd448c9793997aa031a4c5b38871
CoffeeDeveloper <coffeedeveloper@gmail.com>
2015-02-27 16:49:49 +0800

    atom shell

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-var app = require('app')
-
+var app           = require('app')
 var BrowserWindow = require('browser-window')
 
 var mainWindow = null
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.loadUrl('file://' + __dirname + '/app.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
